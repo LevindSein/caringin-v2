8453d843a035e048c5b1b017de858f54fcecf99b
Levind Sein <levindsein@gmail.com>
2021-04-27 00:20:16 +0700

    Notif

--- public/js/tagihan/tagihan.js
+++ public/js/tagihan/tagihan.js
@@ -38,15 +38,54 @@ $(document).ready(function(){
             dataType:"json",
             success:function(data)
             {
+                var reportCookie = getCookie('report');
+
                 $('.badge-listrik').html(data.result.listrik);
                 $('.badge-air').html(data.result.air);
                 $('.badge-report').html(data.result.report);
+                if(data.result.report > 0){
+                    if(data.result.report != reportCookie){
+                        $.notify({
+                            icon: 'fas fa-bell',
+                            title: 'Report',
+                            message: data.result.report + ' Tagihan perlu di cek',
+                        },{
+                            type: "info",
+                            animate: { enter: "animated fadeInRight", exit: "animated fadeOutRight" },
+                        })
+                    }
+                }
+                
+                setCookie('report', data.result.report, 1);
             }
         })
     }
     setInterval(function(){ 
         badge()
     }, 5000);
+
+    function setCookie(name,value,days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days*24*60*60*1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    }
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+    function eraseCookie(name) {   
+        document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
 
     $(document).on('click', '.synchronize', function(){
 		$('.titles').text("Sinkronisasi Tagihan Periode " + periode + " ?");
